dd0c7bfef6fac86a1a23deaaa01839ff8518dea8
gww_555 <9897202+gww_555@user.noreply.gitee.com>
2022-02-26 16:37:29 +0800

    v1

---
+++ MOOC1.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+    int num,line;
+    char c;
+    scanf("%d %c",&num,&c);
+    for(line=1;2*line*line-1<=num;line++);
+    line--;
+    for(int i=1;i<=line;i++)//Ç°lineĞĞ£¨µ¹Èı½Ç£©
+    {
+        for(int j=1;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=2*(line-i+1)-1;j++)
+        {
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+    for(int i=1;i<line;i++)
+    {
+        for(int j=1;j<line-i;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=2*i+1;j++)
+        {
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+    printf("%d\n",num-(2*line*line-1));
+    system("pause");
+    return 0;
+}
